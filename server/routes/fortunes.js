7b4dea0e9f0102e3e98798330cfbabfe0b530968
hjsith <hasithjayasekera@gmail.com>
2021-07-25 19:12:38 +1000

    Fix for missing code and mongodb connection

--- server/routes/fortunes.js
+++ server/routes/fortunes.js
@@ -1,8 +1,10 @@
-const router = require("express").Router;
-const fortuneController = require("../controllers/fortuneController");
+const router = require("express").Router();
+const fortuneController = require("../controllers/fortuneControllers");
 
-router.get("/all", fortuneController.getAllFortune);
+router.get("/all", fortuneController.getAllFortunes);
 router.get("/random", fortuneController.getRandomFortune);
 router.post("/", fortuneController.createNewFortune);
 router.put("/:id", fortuneController.updateExistingFortune);
 router.delete("/:id", fortuneController.deleteExistingFortune);
+
+module.exports = router;
