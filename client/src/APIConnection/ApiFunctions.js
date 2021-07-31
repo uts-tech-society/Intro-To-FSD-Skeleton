bced9c26f56803eafb572f59cffc45bc2ad2edb8
Rachel Hsia <rachel.hsia@student.uts.edu.au>
2021-07-31 15:03:18 +1000

    skeleton frontend created

--- client/src/APIConnection/ApiFunctions.js
+++ client/src/APIConnection/ApiFunctions.js
@@ -8,56 +8,26 @@ const baseurl = "http://localhost:3001/api/fortunes";
 //   ]
 
 // read fortunes (GET)
-export async function getFortunes() {
-    const url = baseurl + "/all";
-    let results = await fetch(url).then(res => res.json())
-    console.log(results)
-    return results;
+function getFortunes() {
+
 }
 
 // get one random fortune
-export async function getRandomFortune() {
-    const url = baseurl + "/random";
-    let results = await fetch(url).then(res => res.json())
-    console.log(results)
-    return results;
+function getRandomFortune() {
+    
 }
 
 // create fortune (POST)
-export async function createFortune(fortuneText) {
-    const requestPayload = {
-        method: "POST",
-        body: JSON.stringify({"fortuneName": fortuneText}),
-        headers: {
-            'Content-Type': 'application/json'
-          },
-    }
-    const url = baseurl + "/";
-    console.log(requestPayload)
-    await fetch(url, requestPayload).then(data => console.log(data));
+function createFortune() {
+
 }
 
 //update fortune (PATCH)
-export async function updateFortune(fortuneId, newText) {
-    const requestPayload = {
-        method: "PUT", 
-        body: JSON.stringify({ "_id": fortuneId, "fortuneName": newText }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }
-    const url = baseurl + "/";
-    await fetch(url, requestPayload).then(data => console.log(data));
+function updateFortune() {
+
 }
 
 // delete fortune (DELETE)
-export async function deleteFortune(fortuneId) {
-    const requestPayload = {
-        method: "DELETE"
-    }
-    const url = baseurl + "/" + fortuneId;
-    await fetch(url, requestPayload).then(data => console.log(data));
+function deleteFortune() {
+
 }
-
-
-
--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,11 @@
-import React, {useState, useEffect} from "react";
-import SeeAllFortunes from "./Components/SeeAllFortunes.js";
-import OpenFortune from "./Components/OpenFortune.js";
-import {getFortunes} from './APIConnection/ApiFunctions.js';
+import React from "react";
 import './App.css';
 
 function App() {
-  
-  const [fortunes, setFortunes] = useState([]);
-
-  useEffect(() => {
-    getFortunes().then((fortunes) => {
-      setFortunes(fortunes);
-    });
-  }, [])// remove [] for live updating
 
   return (
     <div className="App">
-      <OpenFortune fortunes={fortunes}/>
-      <SeeAllFortunes fortunes={fortunes}/>
+      <pre>Fortune cookie coming soon....</pre>
     </div>
   );
 }
